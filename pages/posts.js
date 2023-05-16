f57dc66af14ef3d72107993acd69b6d5c7088225
DHARM PATEL <103331241+DharmPatel02@users.noreply.github.com>
2023-05-17 00:02:19 +0530

    Change Title

--- pages/posts.js
+++ pages/posts.js
@@ -1,26 +1,30 @@
-import React from 'react'
-import PostCard from '../Components/PostCard'
-import Navbar from '../Components/Navbar'
-import { ChakraProvider } from '@chakra-ui/react'
-import MemeCard from '../Components/MemeCard'
+import React from "react";
+import PostCard from "../Components/PostCard";
+import Navbar from "../Components/Navbar";
+import { ChakraProvider } from "@chakra-ui/react";
+import MemeCard from "../Components/MemeCard";
+import Head from "next/head";
 
 const Posts = () => {
   return (
     <div>
-    <ChakraProvider>
-    <Navbar/>
-    {/* <Card/> */}
-    <MemeCard/>
-    <MemeCard/>
-    <MemeCard/>
-    <MemeCard/>
-    <MemeCard/>
-    <PostCard/>
-    <PostCard/>
-    <PostCard/>
-  </ChakraProvider>
+      <Head>
+        <title>Memes App</title>
+      </Head>
+      <ChakraProvider>
+        <Navbar />
+        {/* <Card/> */}
+        <MemeCard />
+        <MemeCard />
+        <MemeCard />
+        <MemeCard />
+        <MemeCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </ChakraProvider>
     </div>
-  )
-}
+  );
+};
 
-export default Posts
+export default Posts;
